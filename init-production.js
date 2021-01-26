10a61067f1fbee1a60032c5960334d14612b644e
benmarzouk <benmarzouk@benmarzouk-pc.home>
2021-01-26 22:35:20 +0100

    tech(production): dockerize backend app

---
+++ init-production.js
@@ -0,0 +1,22 @@
+db = new Mongo().getDB("atol");
+
+// product collection
+db.createCollection("products");
+
+// sequence used to augenerat _id 
+db.createCollection("counters");
+
+// init some data into products collection
+db.products.insert(
+    [
+        { "_id": 1, "name": "AC1 Phone1", "type": "phone", "price": 200.05, "rating": 3.8, "warranty_years": 1, "available": true },
+        { "_id": 2, "name": "AC2 Phone2", "type": "phone", "price": 147.21, "rating": 1, "warranty_years": 3, "available": false },
+        { "_id": 3, "name": "AC3 Phone3", "type": "phone", "price": 150, "rating": 2, "warranty_years": 1, "available": true },
+        { "_id": 4, "name": "AC4 Phone4", "type": "phone", "price": 50.20, "rating": 3, "warranty_years": 2, "available": true }
+    ]);
+
+// init _id sequence
+db.counters.insert({
+    "_id": "productid",
+    "sequence_value": 5
+})
